242ce1f5148b4af4eee14441aa0a9081067d5396
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-08 09:23:41 -0800

    Built initial question and helper functions, testing

--- AboutMe/AboutMe/Program.cs
+++ AboutMe/AboutMe/Program.cs
@@ -6,7 +6,52 @@ namespace AboutMe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int correct = 0;
+            int incorrect = 0;
+            Console.WriteLine("Here's a quick quiz about me!");
+            if (SibNum(correct+incorrect))
+            {
+                correct++;
+            }
+            else
+            {
+                incorrect++;
+            }
+            Console.WriteLine("You got " + correct + " questions right and " + incorrect + " questions incorrect.");
+            Console.ReadLine();
+        }
+
+        static bool SibNum(int questionNumber)
+        {
+            WriteQuestionNumber(questionNumber);
+            Console.WriteLine("How many siblings do I have?");
+            string userInput = Console.ReadLine();
+            int parsedInput = UserInt(userInput);
+            return (parsedInput == 1);
+        }
+
+        static void WriteQuestionNumber(int questionNumber)
+        {
+            Console.WriteLine("Question #" + (questionNumber + 1) + ": ");
+        }
+
+        static int UserInt(string userInput)
+        {
+            int parsedInt = 0;
+            try
+            {
+                parsedInt = Convert.ToInt32(parsedInt);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Format exception thrown - are you sure that string was convertable to an integer?");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Hit an unexpected exception:");
+                Console.WriteLine(e.Message);
+            }
+            return parsedInt;
         }
     }
 }
